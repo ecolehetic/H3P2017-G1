d7fee8b9e43e5a88f0ec3f6b3ef74184de210331
fpumir <fpumir@terredeweb.fr>
2014-10-09 14:38:13 +0200

    exercice bubbling complete
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- video/js/video.js
+++ video/js/video.js
@@ -2,14 +2,21 @@
 
 var video=document.getElementById('video');
 var button=document.getElementById('button');
+var pB=document.getElementById('progressBar');
+
 video.load();
 button.classList.add('loading');
 
 video.addEventListener('canplaythrough',playPause,false);
 window.addEventListener('click',playPause,false);
 video.addEventListener('timeupdate',playProgress,false);
+pB.addEventListener('click',setVideoTime,false);
 
 function playPause(e){
+	if(e.type=='canplaythrough'){
+		video.removeEventListener('canplaythrough',playPause,false);
+	}
+	console.log(e); 
 	button.classList.remove('loading');
 	if(video.paused){
 		video.play();
@@ -25,6 +32,12 @@ function playProgress(){
 	var self=this;
 	var progress=self.currentTime*100/self.duration;
 	document.querySelector('.progress').style.width=progress+'%';
+}
+
+function setVideoTime(e){
+	e.stopPropagation();
+	console.log(e); 
+	video.currentTime=e.offsetX*video.duration/this.offsetWidth;
 }
 
 
